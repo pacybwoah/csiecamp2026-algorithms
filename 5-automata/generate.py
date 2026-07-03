182731095320171e17950898f3aacec8e57413db
Pacybwoah <58631368+pacybwoah@users.noreply.github.com>
2026-07-03 12:05:39 +0800

    feat: modified editorial for 5 and change deck size to 12

--- 5-automata/generate.py
+++ 5-automata/generate.py
@@ -1,15 +1,15 @@
 import random
 while True:
-    number = random.randint(0, 1023)
+    number = random.randint(0, 2 ** 12 - 1)
     arr = []
-    for i in range(0, 10):
+    for i in range(0, 12):
         if number % 2 == 1:
             arr.append(1)
         else:
             arr.append(0)
         number //= 2
     cnt = 0
-    for i in range(0, 8):
+    for i in range(0, 10):
         if arr[i] == 1 and arr[i + 1] == 0 and arr[i + 2] == 1:
             cnt += 1
     if cnt > 0:
